7d50094323cc0011c573b3c7b6b2b6c158222541
ssangyoungyuwon <ssangyoungyuwon@gmail.com>
2026-08-06 21:04:09 +0900

    fix: assign daily quiz with POST

--- src/main/java/com/triread/api/quiz/QuizController.java
+++ src/main/java/com/triread/api/quiz/QuizController.java
@@ -10,7 +10,6 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,7 +26,7 @@ public class QuizController {
         this.quizService = quizService;
     }
 
-    @GetMapping("/today")
+    @PostMapping("/today")
     public QuizService.TodayQuizResponse today(
             @AuthenticationPrincipal AuthPrincipal principal
     ) {
